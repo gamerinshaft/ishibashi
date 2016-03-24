e88c9b2ff84d0e76f85137cbc0c080166db431e5
j.taneichi <j.tane3ma@gmail.com>
2016-03-24 16:41:07 +0900

    update ishibashi.py

--- ishibashi.py
+++ ishibashi.py
@@ -1,2 +1,24 @@
-def hello():
-    print("Hello, World")
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+
+class Ishibashi:
+
+    def __init__(self, mode="bcm"):
+      if mode is "bcm":
+        GPIO.setmode(GPIO.BCM)
+      elif mode is "pin":
+        GPIO.setmode(GPIO.BOARD)
+      else:
+        raise Exception("No such PinMode!")
+
+    def should_be_high(self, gpio):
+      return 'HIGH'
+
+    def should_be_low(self, gpio):
+      return 'LOW'
+
+    def check_volt():
+      return 'checking volt'
+
+

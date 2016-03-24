9478ef9ada3697ec9084e63a08620b8996b77c1c
j.taneichi <j.tane3ma@gmail.com>
2016-03-24 16:42:25 +0900

    add unit_test.py

---
+++ unit_test.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import os, sys, unittest
+from ishibashi import Ishibashi
+
+class SampleTest(unittest.TestCase):
+    """あるクラスをテストするテストクラス"""
+    CLS_VAL = 'none'
+
+    # テストクラスが初期化される際に一度だけ呼ばれる (python2.7以上)
+    @classmethod
+    def setUpClass(cls):
+        if sys.flags.debug: print('> setUpClass method is called.')
+        # テストの準備するための重い処理のメソッドを実行
+        cls.CLS_VAL = '> setUpClass : initialized!'
+        if sys.flags.debug: print(cls.CLS_VAL)
+
+    # テストクラスが解放される際に一度だけ呼ばれる (python2.7以上)
+    @classmethod
+    def tearDownClass(cls):
+        if sys.flags.debug: print('> tearDownClass method is called.')
+        # setUpClassで準備したオブジェクトを解放する
+        cls.CLS_VAL = '> tearDownClass : released!'
+        if sys.flags.debug: print(cls.CLS_VAL)
+
+    # テストメソッドを実行するたびに呼ばれる
+    def setUp(self):
+        if sys.flags.debug: print(os.linesep + '> setUp method is called.')
+        # テストの準備をするための軽い処理を実行
+        self.smpl = Ishibashi("bcm")
+
+    # テストメソッドの実行が終わるたびに呼ばれる
+    def tearDown(self):
+        if sys.flags.debug: print(os.linesep + '> tearDown method is called.')
+        # setUpで準備したオブジェクトを解放する
+
+    def test_hoge(self):
+        expected = 'HIGH'
+        actual = self.smpl.should_be_high(20)
+        self.assertEqual(expected, actual)
+
+    def test_poyo(self):
+        expected = 'LOW'
+        actual = self.smpl.should_be_low(20) # 凡ミス
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    # unittestを実行
+    unittest.main()
